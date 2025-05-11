2c4a425050adebcf2942dcd21bc3ba1777207cbb
jitendra kumar <jitendra238@gmail.com>
2025-05-11 12:36:07 +0530

    docker file environment variable added

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -12,10 +12,18 @@ namespace eCommerce.ProductsService.DataAccessLayer
         public static IServiceCollection AddDataAccessLayer(this IServiceCollection services,
             IConfiguration configuration)
         {
+            string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+            string connectionString = connectionStringTemplate
+                .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+                .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"))
+                .Replace("$MYSQL_USER", Environment.GetEnvironmentVariable("MYSQL_USER"))
+                .Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT"))
+                .Replace("$MYSQL_DATABASE", Environment.GetEnvironmentVariable("MYSQL_DATABASE"));
+
             //To Do : Add Data Access Layer Servie into the IOC container
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+                options.UseMySQL(connectionString);
             });
             services.AddScoped<IProductsRepository, ProductsRepository>();
             return services;
